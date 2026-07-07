0e4cf46c2f45a28c7cbcc66d72d5d415caefe69f
Venkatesh Prathipati <34228239+venkateshprathipati@users.noreply.github.com>
2026-07-07 09:54:45 +0530

    Initial commit

---
+++ src/main/java/com/novalabs/digitalbanking/DigitalbankingApplication.java
@@ -0,0 +1,13 @@
+package com.novalabs.digitalbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novalabs/digitalbanking/DigitalbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novalabs.digitalbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
