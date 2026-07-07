f869495981054b6b4a50d2176f0e08c9c1732e13
Venkatesh Prathipati <34228239+venkateshprathipati@users.noreply.github.com>
2026-07-07 15:35:22 +0530

    Added profiles and property files

--- src/main/java/com/novalabs/digitalbanking/DigitalbankingApplication.java
+++ src/main/java/com/novalabs/digitalbanking/DigitalbankingApplication.java
@@ -2,8 +2,11 @@ package com.novalabs.digitalbanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DigitalbankingApplication {
 
 	public static void main(String[] args) {
